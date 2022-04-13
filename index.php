b96973086dff19e76108044093fc7231e8e4ea4b
Khaliza Fania <khlzfan03@gmail.com>
2022-04-13 10:08:51 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates libero ad rem optio quidem ipsam totam magnam tempore quas et provident, architecto fugiat sint ab temporibus, repellat vitae quis dicta.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
